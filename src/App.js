bc60e04ec7fd7e651b1edfb0a0454bae0f5f5288
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-16 11:40:44 +0300

    Fetch cart items with redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { getCartItems } from "./redux/features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems, isLoading, error } = useSelector(
+    (store) => store.cart
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoding) {
+    return <h1>Loading...</h1>;
+  }
+  if (!isLoading && error) {
+    return <h1>Threre was an error!</h1>;
+  }
   return (
     <main>
       <Navbar />
--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -1,12 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from "../../../cartItems";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
   cartItems: cartItems,
   amount: 5,
   total: 0,
   isLoading: true,
+  error: false,
 };
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  () => {
+    return fetch(url).then((res) =>
+      res.json().catch((err) => console.log(err))
+    );
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -20,6 +32,19 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter(
         (item) => item.id !== itemId
       );
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.pending]: (state) => {
+      state.isLoading = false;
+      state.error = true;
     },
   },
 });
